f5a2324b5ce75a58c0e30fb0d414800ec7f8bba7
Goldenberg <rafaelgo@kth.se>
2016-04-18 16:06:49 +0200

    Pushing Answers to Firebase

--- app/scripts/controllers/newquiz.js
+++ app/scripts/controllers/newquiz.js
@@ -10,11 +10,11 @@
 angular.module('bdl6App')
   .controller('NewquizCtrl', function ($scope, $firebaseObject, $firebaseArray, $routeParams, Ref, Auth) {
   	//$scope.quizID = $routeParams.quizID;
-  	var quizID = $routeParams.quizID;
+  	$scope.quizID = $routeParams.quizID;
   	var questionArrayKey;
   	$scope.answerArray = [];
   	//Set-up quiz reference and binding
-  	var quizRef = Ref.child('Quiz/' + Ref.getAuth().uid + '/' + quizID);
+  	var quizRef = Ref.child('Quiz/' + Ref.getAuth().uid + '/' + $scope.quizID);
   	var quizObj = $firebaseObject(quizRef);
 	quizObj.$bindTo($scope, 'quiz');
 	
@@ -26,18 +26,28 @@ angular.module('bdl6App')
 	//Question scope variables
 	$scope.questionName;
 	$scope.questionImg = "";
-	$scope.questionTime;
+	$scope.questionTime = 1;
 	$scope.questionType = "multiple";
 
+	$scope.changedQuestion = function(){
+
+	}
+
 	$scope.addAnswerToArray = function(answer){
+		//var jsonString = angular.toJson(answer);
+		//console.log(jsonString);
+		//console.log(answer);
 		$scope.answerArray.push(answer);
-		console.log($scope.answerArray);
+		console.log(JSON.parse(angular.toJson($scope.answerArray)));
 	}
 	//Add question to array
 	$scope.addQuestion = function(){
 		var ref = Ref.child('Question/' + questionArrayKey);
 		var qid = ref.push({Image: $scope.questionImg, Name: $scope.questionName, Time: $scope.questionTime, Type: $scope.questionType});
 		var aref = Ref.child('Answer/' + questionArrayKey + '/' + qid.key());
+		aref.push(JSON.parse(angular.toJson($scope.answerArray)));
+		//for (var i = 0; i < $scope.answerArray.length; i++)
+		//	aref.push(JSON.parse(angular.toJson($scope.answerArray[i])));
 		//var qArray = $firebaseArray(ref);
 		//qArray.$add({Image: $scope.questionImg, Name: $scope.questionName, Time: $scope.questionTime, Type: $scope.questionType});
 	}
